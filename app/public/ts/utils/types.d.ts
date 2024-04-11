fa64a3da2dc3849618ad12e1ad452fbf08e737b8
mi-hye <satge13@gmail.com>
2024-04-11 11:55:49 +0900

    feat: Implement type declaration file

---
+++ app/public/ts/utils/types.d.ts
@@ -0,0 +1,10 @@
+interface NewsItem {
+	id: string;
+	author: null | string;
+	content: string;
+	description: string;
+	publishedAt: string;
+	title: string;
+	url: string;
+	urlToImage: string;
+}
