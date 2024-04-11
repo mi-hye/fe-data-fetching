bf657bd945a132dad8ceeb70c7693ce13e1e4369
mi-hye <satge13@gmail.com>
2024-04-11 21:34:45 +0900

    feat: Implement timer

--- app/public/ts/main.ts
+++ app/public/ts/main.ts
@@ -1,5 +1,9 @@
+import delay from "./utils/delay.js";
+import { onClickRefresh, setTimer } from "./views/header.js";
 import renderNewsDes from "./views/newsDescription.js";
 import { renderTitle, onClickNewsTitle } from "./views/newsTitle.js";
 
 await renderTitle();
-onClickNewsTitle(renderNewsDes);
+onClickNewsTitle(renderNewsDes, delay);
+onClickRefresh(renderTitle,delay);
+setTimer();
