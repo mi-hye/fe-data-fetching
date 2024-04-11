d39c1aefef1cc76a2c1d122316ab0ca41e11178d
mi-hye <satge13@gmail.com>
2024-04-11 20:25:02 +0900

    feat: Implement delay

--- app/public/ts/views/newsTitle.ts
+++ app/public/ts/views/newsTitle.ts
@@ -1,5 +1,7 @@
 import { fetchTotalNews, fetchSingleNews } from "../apis/news.js";
 
+const DELAY = 2000;
+
 async function renderTitle() {
 	const $newsWrap = document.querySelector(".news-main__title-list-wrap") as HTMLElement;
 	const newsList = await fetchTotalNews();
@@ -28,8 +30,24 @@ async function handleClickNewsTitle(e: MouseEvent, render: Function) {
 	if ($target.tagName === "A") {
 		const id: string = $target.dataset.id ?? "";
 		const news = await fetchSingleNews(id);
+		await delay();
 		if (news) render(news);
 	}
+}
+
+function delay() {
+	const $newsWrap = document.querySelector(".news") as HTMLElement;
+	const $loading = document.querySelector(".loading") as HTMLElement;
+
+	$newsWrap.classList.add("blur");
+	$loading.style.display = "block";
+
+	return new Promise<void>((resolve) => {
+		setTimeout(resolve, DELAY);
+	}).then(() => {
+		$newsWrap.classList.remove("blur");
+		$loading.style.display = "none";
+	});
 }
 
 export { renderTitle, onClickNewsTitle };
